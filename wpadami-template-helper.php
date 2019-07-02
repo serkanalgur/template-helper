ad8565a3359fcc8f1602ab18a26fbdc0247d35de
Serkan Algur <info@wpadami.com>
2019-07-03 00:11:13 +0400

    🚀 Initial Release on Github

---
+++ wpadami-template-helper.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * WPAdamı Template Helper
+ *
+ * @package   WPAdami_Template_Helper
+ * @author    Gary Jones <gary@garyjones.co.uk>
+ * @license   GPL-3.0+
+ * @link      https://github.com/serkanalgur/wpadami-template-helper
+ * @copyright 2019 Serkan Algur, WPAdamı
+ *
+ * @wordpress-plugin
+ * Plugin Name:       WPAdamı Template Helper
+ * Plugin URI:        https://github.com/serkanalgur/wpadami-template-helper
+ * Description:       Template helper by Serkan Algur
+ * Version:           1.0.0
+ * Author:            Serkan Algur
+ * Author URI:        https://github.com/serkanalgur
+ * Text Domain:       wpadami-template-helper
+ * License:           GPL-3.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-3.0.txt
+ * Domain Path:       /languages
+ * GitHub Plugin URI: https://github.com/serkanalgur/wpadami-template-helper
+ * GitHub Branch:     master
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
