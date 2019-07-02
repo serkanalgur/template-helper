e60f010768f268dca281c79c30d2ccea6dc797c9
Serkan Algur <info@wpadami.com>
2019-07-03 00:26:21 +0400

    📦 correct_utf_chars_filename function added

--- wpadami-template-helper.php
+++ wpadami-template-helper.php
@@ -22,9 +22,36 @@
  * Domain Path:       /languages
  * GitHub Plugin URI: https://github.com/serkanalgur/wpadami-template-helper
  * GitHub Branch:     master
- */
+*/
 
 // If this file is called directly, abort.
 if ( ! defined( 'WPINC' ) ) {
 	die;
+}
+
+// Begin
+
+if ( ! class_exists( 'WPAdami_Template_Helper', true ) ) {
+	class WPAdami_Template_Helper {
+
+		public function __construct() {
+
+			add_filter( 'wp_handle_upload_prefilter', array( $this, 'correct_utf_chars_filename' ) );
+		}
+
+		/**
+		 * correct wrong chars on filenames
+		 *
+		 * @since 1.0.0
+		*/
+
+		public function correct_utf_chars_filename() {
+
+			$find_utf_8   = array( 'İ', 'Ü', 'Ğ', 'Ö', 'Ç', 'Ş', 'ş', 'ç', 'ö', 'ğ', 'ü', 'ı', ' ' ); // Turkish chars for now
+			$change_utf_8 = array( 'I', 'U', 'G', 'O', 'C', 'S', 's', 'c', 'o', 'g', 'u', 'i', '_' ); // Turkish chars for now
+			$file['name'] = strtolower( str_replace( $find_utf_8, $change_utf_8, $file['name'] ) );
+			return $file;
+		}
+
+	}
 }
