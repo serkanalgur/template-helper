931a6de223f4a5cc6ce478dbf950f63099c11e2c
Serkan Algur <info@wpadami.com>
2019-07-03 12:42:01 +0400

    📦 Emojis Removed

--- wpadami-template-helper.php
+++ wpadami-template-helper.php
@@ -40,6 +40,9 @@ if ( ! class_exists( 'WPAdami_Template_Helper', true ) ) {
 			add_filter( 'wp_handle_upload_prefilter', array( __CLASS__, 'correct_utf_chars_filename' ) );
 			add_action( 'dashboard_glance_items', array( __CLASS__, 'cpt_to_dashboard_info' ) );
 			add_action( 'wp_dashboard_setup', array( __CLASS__, 'disable_default_dashboard_widgets' ), 999 );
+			add_action( 'init', array( __CLASS__, 'disable_emojis_in_theme_wpa' ) );
+			add_filter( 'tiny_mce_plugins', array( __CLASS__, 'disable_emojis_tinymce_wpa' ) );
+			add_filter( 'wp_resource_hints', array( __CLASS__, 'disable_emojis_remove_dns_prefetch_wpa' ), 10, 2 );
 		}
 
 		/**
@@ -115,6 +118,48 @@ if ( ! class_exists( 'WPAdami_Template_Helper', true ) ) {
 			// gravity forms
 			unset( $wp_meta_boxes['dashboard']['normal']['core']['rg_forms_dashboard'] );
 		}
+
+		/**
+		 * remove WordPress Emoji from theme
+		 *
+		 * @since 1.0.0
+		*/
+
+		public function disable_emojis_in_theme_wpa() {
+			remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+			remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+			remove_action( 'wp_print_styles', 'print_emoji_styles' );
+			remove_action( 'admin_print_styles', 'print_emoji_styles' );
+			remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+			remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
+			remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+		}
+
+		public function disable_emojis_tinymce_wpa( $plugins ) {
+			if ( is_array( $plugins ) ) {
+				return array_diff( $plugins, array( 'wpemoji' ) );
+			}
+
+			return array();
+		}
+
+		public function disable_emojis_remove_dns_prefetch( $urls, $relation_type ) {
+
+			if ( 'dns-prefetch' == $relation_type ) {
+
+				// Strip out any URLs referencing the WordPress.org emoji location
+				$emoji_svg_url_bit = 'https://s.w.org/images/core/emoji/';
+				foreach ( $urls as $key => $url ) {
+					if ( strpos( $url, $emoji_svg_url_bit ) !== false ) {
+						unset( $urls[ $key ] );
+					}
+				}
+			}
+
+			return $urls;
+		}
+
+
 
 	}
 }
